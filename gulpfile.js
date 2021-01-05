d41cae15e2dbd2080c543d33f8e890b2e6faa6fa
atebin <a.tebin@gmail.com>
2021-01-05 12:12:08 +0300

    Автоматизация сборки личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const modifyCssUrls = require("gulp-modify-css-urls");
 
 // Styles
 
@@ -19,7 +20,7 @@ const styles = () => {
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
-}
+  }
 
 exports.styles = styles;
 
@@ -49,3 +50,57 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+// **********************************************************************
+
+// Build
+
+const copyHtml = () => {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("build"))
+}
+
+const copyCss = () => {
+  return gulp.src(["source/css/*.css", "source/css/*.css.map"])
+    .pipe(gulp.dest("build/css"))
+}
+
+const copyFonts = () => {
+  return gulp.src(["source/fonts/*.woff", "source/css/*.woff"])
+    .pipe(gulp.dest("build/fonts"))
+}
+
+const copyJs = () => {
+  return gulp.src(["source/js/*.css", "source/js/*.js.map"])
+    .pipe(gulp.dest("build/js"))
+}
+
+const copyImg = () => {
+  return gulp.src("source/img/**/*.*")
+    .pipe(gulp.dest("build/img"))
+}
+
+const stylesbuild = () => {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(modifyCssUrls({
+      modify: (url) => {
+        return url.replace("../../img/", "../img/");
+      },
+    }))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(sync.stream());
+}
+
+// pathincss
+
+
+exports.build = gulp.series(stylesbuild, gulp.parallel(copyHtml, copyCss, copyFonts, copyJs, copyImg));
+
+// **********************************************************************
